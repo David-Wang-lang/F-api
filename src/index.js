944f104bfa6649f8f13dd6965b072283f951fe34
david <david.work.990@gmail.com>
2026-07-09 06:44:26 +0800

    user auth and logger submodules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("F-api started");
+const { login } = require("../libs/F-sub1");
+const { log } = require("../libs/F-sub2");
+
+const result = login("jack");
+
+log(result);
+log("F-api is running");
