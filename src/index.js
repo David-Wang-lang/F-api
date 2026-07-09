5f5135f7caeb8e27774eefb9637f5915b2b44be0
david <david.work.990@gmail.com>
2026-07-09 08:45:15 +0800

    Use get current user from auth submodule

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const { login, logout } = require("../libs/F-sub1");
+const { login, logout, getCurrentUser } = require("../libs/F-sub1");
 const { log } = require("../libs/F-sub2");
 
 const loginResult = login("jack");
 const logoutResult = logout("jack");
+const user = getCurrentUser();
 
 log(loginResult);
 log(logoutResult);
+log(`Current user: ${user.username}, role: ${user.role}`);
 log("F-api is running");
